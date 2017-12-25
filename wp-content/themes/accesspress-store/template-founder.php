34a786d157d4ead4e2aa0d64bff725fb6a4b3fa9
amitgin <amit@amitg.in>
2017-12-25 23:09:39 +0530

    Template added

--- wp-content/themes/accesspress-store/template-founder.php
+++ wp-content/themes/accesspress-store/template-founder.php
@@ -1,32 +1,27 @@
 <?php
 /**
  * This is the front page code
- * Template Name: Founder Template
- * @package AccessPress Store
+ * Template Name: Founders
  */
 get_header();
 
 global $post;
-
-$archive_page_layout = esc_attr( get_post_meta($post->ID, 'accesspress_store_sidebar_layout', true));
-
-if (empty($archive_page_layout)) {
-    $archive_page_layout = esc_attr( get_theme_mod('archive_page_layout','right-sidebar') );
+$single_page_layout = esc_attr( get_post_meta($post->ID, 'accesspress_store_sidebar_layout', true) );
+if (empty($single_page_layout)) {
+    $single_page_layout = esc_attr( get_theme_mod('single_page_layout','right-sidebar') );
 }
-
 if (is_page('cart') || is_page('checkout')) {
-    $archive_page_layout = "no-sidebar";
+    $single_page_layout = "no-sidebar";
 }
-
 $breadcrumb = intval( get_theme_mod('breadcrumb_options_page','1') );
 $archive_bread = esc_url( get_theme_mod('breadcrumb_page_image') );
 if($archive_bread){
     $bread_archive = $archive_bread;
 }else{
-  $bread_archive = esc_url( get_template_directory_uri().'/images/about-us-bg.jpg' );
+  $bread_archive = esc_url ( get_template_directory_uri().'/images/about-us-bg.jpg' );
 }
-
-if($breadcrumb == '1') { ?>
+if($breadcrumb == '1') :
+?>
 <div class="page_header_wrap clearfix" style="background:url('<?php echo $bread_archive; ?>') no-repeat center; background-size: cover;">
     <div class="ak-container">
         <header class="entry-header">
@@ -35,118 +30,104 @@ if($breadcrumb == '1') { ?>
         <?php accesspress_breadcrumbs() ?>
     </div>
 </div>
-<?php } ?>
-
+<?php endif; ?>
 <div class="inner">
-    <main id="main" class="site-main clearfix <?php echo $archive_page_layout; ?>">
-        <?php if ($archive_page_layout == 'both-sidebar'): ?>
+    <main id="main" class="site-main clearfix <?php echo $single_page_layout; ?>">
+        <?php if ($single_page_layout == 'both-sidebar'): ?>
             <div id="primary-wrap" class="clearfix">
         <?php endif; ?>
 
-            <div id="primary" class="content-area <?php echo esc_attr( get_theme_mod('blog_post_layout') ); ?> ">
-                <?php
-                $exclude_cat = get_theme_mod('blog_exclude_categories');
-                $exclude_cat = explode(',', $exclude_cat);
-                $args = array('post_type' => 'post',
-                  'posts_per_page' => 10,
-                  'category__not_in' => $exclude_cat,
-                );
-                $query = new WP_Query($args);
-                if ($query->have_posts()): while ($query->have_posts()) : $query->the_post();
-                ?>
-                <?php
-                $blog_post_layout = esc_attr( get_theme_mod('blog_post_layout') );
-                if (has_post_thumbnail()){
-                    switch ($blog_post_layout) {
-                        case 'blog_layout1':
-                        $image_size = 'accesspress-blog-big-thumbnail';
-                        break;
-                        case 'blog_layout2':
-                        $image_size = 'accesspress-service-thumbnail';
-                        break;
-                        case 'blog_layout3':
-                        $image_size = 'accesspress-service-thumbnail';
-                        break;
-                        case 'blog_layout4':
-                        $image_size = 'accesspress-blog-big-thumbnail';
-                        break;
-                        default:
-                        $image_size = 'accesspress-blog-big-thumbnail';
-                        break;
-                    }
-                    $image = wp_get_attachment_image_src(get_post_thumbnail_id(), $image_size);
+<div id="primary" class="content-area">
+<!-- **************************** Custom Code Start ******************************************-->
+    <style type="text/css">
+        @media (min-width: 600px) {
+            .founder-box {
+                display: block;
+            }
+            .founder-wrap {
+                text-align: justify;
+                padding: 0px 10px;
+                float: left;
+            }
+            .founder-wrap p {
+                margin-top: 10px;
+            }
+            .founder {
+                text-align: center;
+                padding: 10px;
+            }
+            .image {
+                float: right;
+                height: 150px;
+                max-width: 150px;
+                border-radius: 75px;
+            }
+        }
 
-                } ?>
-
-                    <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
-                        <div class="content-inner clearfix">
-                            <?php if (has_post_thumbnail()){ ?>
-                                <div class="entry-thumbanil">
-                                    <img src="<?php echo esc_url( $image[0] ); ?>" alt="<?php the_title(); ?>">
-
-                                    <a class="img-over-link" href="<?php the_permalink(); ?>"><span class="thumb_outer"><span class="thumb_inner"><i class="fa fa-link"></i></span></span></a>
-
-                                </div>
-                            <?php } ?>
-                                <div class="blog_desc">
-                                    <header class="entry-header">
-                                        <span class="cat-name">
-                                            <?php
-                                                $category = get_the_category();
-                                                echo $category[0]->cat_name;
-                                             ?>
-                                        </span>
-                                        <?php the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() )), '</a></h2>'); ?>
-
-                                        <?php if ('post' == get_post_type()) : ?>
-                                            <div class="entry-meta">
-                                                <p class="meta-info">
-                                                    <?php echo __('Posted On', 'accesspress-store'); ?>
-                                                    <?php the_time('F j, Y'); ?>
-                                                    <?php echo __('at', 'accesspress-store'); ?>
-                                                    <?php the_time('g:i a'); ?>
-                                                    <?php echo __('by', 'accesspress-store'); ?>
-                                                    <?php the_author_posts_link(); ?> /  <?php comments_popup_link('No Comments', '1 Comment', '% Comments'); ?>
-                                                </p>
-                                            </div><!-- .entry-meta -->
-                                        <?php endif; ?>
-                                    </header><!-- .entry-header -->
-                                    <div class="entry-content">
-                                        <div class="desc">
-                                            <?php
-                                            if ($blog_post_layout == 'blog_layout4'):
-                                                echo get_the_content();
-                                            else:
-                                                echo accesspress_letter_count(get_the_content(), '200');
-                                            endif;
-                                            ?>
-                                        </div>
-                                        <a href="<?php the_permalink(); ?>" class="bttn read-more">
-                                            <?php _e('Read More', 'accesspress-store'); ?>
-                                        </a>
-                                    </div><!-- .entry-content -->
-                                </div>
-                        </div>
-                    </article><!-- #post-## -->
-                <?php  endwhile; endif; wp_reset_query(); ?>
-            </div><!-- #primary -->
+        @media (min-width: 1000px) {
+            .founder-box {
+                display: flex;
+            }
+            .founder-wrap {
+                text-align: justify;
+                padding: 0px 20px;
+            }
+            .founder-wrap p {
+                margin-top: 40px;
+            }
+            .founder {
+                text-align: center;
+                padding: 10px;
+            }
+            .image {
+                vertical-align: middle;
+                height: 150px;
+                max-width: 150px;
+                border-radius: 75px;
+            }
+        }
+    </style>
+<div id="" class="founder-box" style="margin-top: 30px;">
+<div id="" class="founder-wrap"><strong>Amit Gautam, Founder and Director</strong> of Lubatoo, has over a decade of experience in Product Management, E-commerce, Analytics, Cloud Computing, Mobile Commerce and go-to Market strategy amongst others. He has demonstrated technical and leadership skills to spearhead the top consumer app, Lubatoo. His farsightedness coupled with his unique ability to connect the dots from strategy to tactics has helped.</div>
+<div id="" class="founder"><a href="https://www.lubatoo.com/wp-content/uploads/2017/02/Amit-Gautam.jpg"><img class="image" src="https://www.lubatoo.com/wp-content/uploads/2017/02/Amit-Gautam.jpg" alt="" width="150" height="150" /></a></div>
+</div>
+<div id="" class="founder-box">
+<div id="" class="founder"><a href="https://www.lubatoo.com/wp-content/uploads/2017/12/Neelam.jpg"><img class="image" src="https://www.lubatoo.com/wp-content/uploads/2017/12/Neelam.jpg" alt="Neelam Gautam" width="150" height="150" /></a></div>
+<div id="" class="founder-wrap"><p><strong>Neelam Gautam, Co-Founder and Director</strong> of Lubatoo, has over a decade of experience in Market Analyst and Team Management. He is also one of sales head in state lavel.</p></div>
+</div>
+<div id="" class="founder-box">
+<div id="" class="founder-wrap"><p><strong>Ravneet Kumar, Co-Founder and Director</strong> of Lubatoo, has over a decade of experience in Market Analyst and Team Management. He is also one of sales head in state lavel.</p></div>
+<div id="" class="founder"><img class="image" src="https://www.lubatoo.com/wp-content/uploads/2017/02/Ravneet.jpg" alt="" width="150" height="150" /></div>
+</div>
+<div>
+<?php
+    $size = "<script>screen.width;</script>";
+    echo $size;
+    if($size>=800){
+        echo "Good Morning !";
+    }else{
+        echo "Good Night !";
+    }
+?>
+</div>
+<!-- **************************** Custom Code End ******************************************-->
+</div><!-- #primary -->
 
             <?php
-                if ($archive_page_layout == 'both-sidebar' || $archive_page_layout == 'left-sidebar'):
+                if ($single_page_layout == 'both-sidebar' || $single_page_layout == 'left-sidebar'):
                     get_sidebar('left');
                 endif;
             ?>
 
-        <?php if ($archive_page_layout == 'both-sidebar'): ?>
+        <?php if ($single_page_layout == 'both-sidebar'): ?>
             </div>
         <?php endif; ?>
 
             <?php
-                if ($archive_page_layout == 'both-sidebar' || $archive_page_layout == 'right-sidebar'):
+                if ($single_page_layout == 'both-sidebar' || $single_page_layout == 'right-sidebar'):
                     get_sidebar('right');
                 endif;
             ?>
-
-    </main><!-- #main -->
+    </main>
 </div>
 <?php get_footer();
